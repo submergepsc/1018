02019208c0b1772137019f3ccbc4a76630dfbe7f
Jianglong Hu <hujlong3@mail2.sysu.edu.cn>
2025-10-18 23:18:20 +0800

    Move polynomial project files into polyV1

---
+++ polyV1/mainwindow.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <QMainWindow>
+#include <QString>
+
+#include "polynomial/polynomial.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow() override;
+
+private slots:
+    void on_addButton_clicked();
+    void on_subtractButton_clicked();
+    void on_multiplyButton_clicked();
+    void on_derivativeButton_clicked();
+    void on_evaluateButton_clicked();
+
+private:
+    bool validatePolynomialInputs(bool requireSecond = true);
+    bool validatePolynomialFormat(const QString &text) const;
+    void showError(const QString &message);
+    QString getPolynomialText(int index) const;
+    Polynomial buildPolynomialFromInput(int index) const;
+    void displayResult(const QString &title, const QString &content);
+    QString polynomialToDisplayString(const Polynomial &poly) const;
+
+private:
+    Ui::MainWindow *ui;
+};
